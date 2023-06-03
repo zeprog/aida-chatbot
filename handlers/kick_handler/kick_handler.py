f60c56b1918cbfb99af719bbc942f07a0a6b3ada
Alina Zelenskaya <dmitrium02@mail.ru>
2023-06-03 18:02:43 +0300

    Can't kick and warn yourself

--- handlers/kick_handler/kick_handler.py
+++ handlers/kick_handler/kick_handler.py
@@ -9,17 +9,24 @@ def handle_kick(vk_session, msg, admin_keys):
   remove_id_without_numbers = re.findall(r'\d+', remove_id)[0]
   fwd_user = utils.get_user_by_id(int(remove_id_without_numbers))
   if int(remove_id_without_numbers) not in admin_keys:
-    if 'id' in msg['text']:
-      vk_session.method('messages.removeChatUser', {
-        'user_id': remove_id_without_numbers,
-        'chat_id': msg['peer_id'] - 2000000000
-      })
-      utils.remove_user_by_id(fwd_user.vk_id)
-      User().delete_by_id(remove_id)
+    if int(remove_id_without_numbers) != fwd_user.vk_id:
+      if 'id' in msg['text']:
+        vk_session.method('messages.removeChatUser', {
+          'user_id': remove_id_without_numbers,
+          'chat_id': msg['peer_id'] - 2000000000
+        })
+        utils.remove_user_by_id(fwd_user.vk_id)
+        User().delete_by_id(remove_id)
+      else:
+        vk_session.method('messages.removeChatUser', {
+          'member_id': -int(remove_id_without_numbers),
+          'chat_id': msg['peer_id'] - 2000000000
+        })
+        utils.remove_user_by_id(-fwd_user.vk_id)
+        User().delete_by_id(remove_id)
     else:
-      vk_session.method('messages.removeChatUser', {
-        'member_id': -int(remove_id_without_numbers),
-        'chat_id': msg['peer_id'] - 2000000000
+      vk_session.method('messages.send', {
+        'chat_id': msg['peer_id'] - 2000000000,
+        'message': 'Вы не можете кикнуть самого себя!',
+        'random_id': 0
       })
-      utils.remove_user_by_id(-fwd_user.vk_id)
-      User().delete_by_id(remove_id)
--- handlers/warns_handler/warns_handler.py
+++ handlers/warns_handler/warns_handler.py
@@ -7,21 +7,28 @@ def handle_warn(vk_session, msg, admin_keys):
   warn_id = re.search(r'(?:id|club)(\d+)', msg['text'])
   warn_id = warn_id.group(0)
   warn_id_without_numbers = int(re.findall(r'\d+', warn_id)[0])
+  fwd_user = utils.get_user_by_id(warn_id_without_numbers)
   if warn_id_without_numbers not in admin_keys:
-    fwd_user = utils.get_user_by_id(warn_id_without_numbers)
-    fwd_user.warns += 1
-    fwd_user.save()
-    
-    vk_session.method('messages.send', {
-      'chat_id': msg['peer_id'] - 2000000000,
-      'message': f'Выдано предупреждение {fwd_user.name}. Предупреждений {fwd_user.warns}/{max_warns}',
-      'random_id': 0
-    })
-    
-    if fwd_user.warns >= max_warns:
-      vk_session.method('messages.removeChatUser', {
-        'user_id': fwd_user.vk_id,
-        'chat_id': msg['peer_id'] - 2000000000
+    if warn_id_without_numbers != fwd_user.vk_id:
+      fwd_user.warns += 1
+      fwd_user.save()
+      
+      vk_session.method('messages.send', {
+        'chat_id': msg['peer_id'] - 2000000000,
+        'message': f'Выдано предупреждение {fwd_user.name}. Предупреждений {fwd_user.warns}/{max_warns}',
+        'random_id': 0
       })
-      utils.remove_user_by_id(fwd_user.vk_id)
-      User().delete_by_id(fwd_user.vk_id)
+      
+      if fwd_user.warns >= max_warns:
+        vk_session.method('messages.removeChatUser', {
+          'user_id': fwd_user.vk_id,
+          'chat_id': msg['peer_id'] - 2000000000
+        })
+        utils.remove_user_by_id(fwd_user.vk_id)
+        User().delete_by_id(fwd_user.vk_id)
+    else:
+      vk_session.method('messages.send', {
+        'chat_id': msg['peer_id'] - 2000000000,
+        'message': 'Вы не можете дать предупреждение самому себе!',
+        'random_id': 0
+      })
