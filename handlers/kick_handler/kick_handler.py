f4c27347765c9fc60deaf177595029e521e07a2d
Alina Zelenskaya <dmitrium02@mail.ru>
2023-10-29 05:07:45 +0300

    Refactoring code

--- handlers/kick_handler/kick_handler.py
+++ handlers/kick_handler/kick_handler.py
@@ -1,55 +1,39 @@
 import re
-import utils
 import json
 from models import User
 from config import *
 from buttons import keyboard
+import utils
 
 ### KICK
 def kick_user(vk_session, msg, user_id):
+  chat_id = msg['peer_id'] - 2000000000
+  
   if int(user_id) > 0:
-    vk_session.method('messages.removeChatUser', {
-      'user_id': user_id,
-      'chat_id': msg['peer_id'] - 2000000000
-    })
-    utils.remove_user_by_id(user_id)
-    User().delete_by_id(user_id)
+    vk_session.method('messages.removeChatUser', {'user_id': user_id, 'chat_id': chat_id})
   else:
-    vk_session.method('messages.removeChatUser', {
-      'member_id': int(user_id),
-      'chat_id': msg['peer_id'] - 2000000000
-    })
-    utils.remove_user_by_id(-user_id)
-    User().delete_by_id(abs(user_id))
+    vk_session.method('messages.removeChatUser', {'member_id': int(user_id), 'chat_id': chat_id})
+      
+  utils.remove_user_by_id(abs(user_id))
+  User().delete_by_id(abs(user_id))
 
 def handle_kick(vk_session, msg, admin_keys, members):
   user_id = None
+  chat_id = msg['peer_id'] - 2000000000
 
   if 'reply_message' in msg:
     fwd = msg['reply_message']
-    if int(fwd['from_id']) > 0:
-      for profile in members['profiles']:
-        utils.get_user_by_profile(profile)
-    else:
-      for group in members['groups']:
-        utils.get_user_by_profile(group)
-
+    utils.get_user_by_profile(fwd['from_id'])
     user_id = fwd['from_id']
   else:
     remove_id = re.search(r'(?:id|club)(\d+)', msg['text'])
-    remove_id = remove_id.group(0)
     if remove_id:
-      remove_id_without_numbers = re.findall(r'\d+', remove_id)[0]
-      user_id = int(remove_id_without_numbers)
+      user_id = int(remove_id.group(1))
 
-  if user_id is not None and user_id != msg['from_id'] and int(user_id) not in admin_keys:
+  if user_id and user_id != msg['from_id'] and user_id not in admin_keys:
     kick_user(vk_session, msg, user_id)
   else:
-    vk_session.method('messages.send', {
-      'chat_id': msg['peer_id'] - 2000000000,
-      'message': 'Вы не можете кикнуть самого себя!',
-      'random_id': 0
-    })
+    vk_session.method('messages.send', {'chat_id': chat_id, 'message': 'Вы не можете кикнуть самого себя!', 'random_id': 0})
     
 # САМОКИК
 def selfkick_answer(vk_session, msg):
@@ -59,20 +43,14 @@ def selfkick_answer(vk_session, msg):
     'random_id': 0,
     'keyboard': keyboard,
   })
-    
-def selfkick(vk_session, msg, event, user_id, kick_user):
-  if user_id not in map(int, admin_keys):
-    vk_session.method('messages.send', {
-      'chat_id': msg['peer_id'] - 2000000000,
-      'message': 'У вас нет уровня доступа для этих команд!',
-      'random_id': 0
-    })
-  else:
-    payload = json.loads(event.object['message']['payload'])
-    if payload.get('id') == '1':
-      vk_session.method('messages.removeChatUser', {
-        'user_id': kick_user,
-        'chat_id': msg['peer_id'] - 2000000000
-      })
-      utils.remove_user_by_id(kick_user)
-      User().delete_by_id(kick_user)
+
+def selfkick(vk_session, msg, event, user_id, kick_user_id):
+  chat_id = msg['peer_id'] - 2000000000
+
+  if user_id not in admin_keys:
+    vk_session.method('messages.send', {'chat_id': chat_id, 'message': 'У вас нет уровня доступа для этих команд!', 'random_id': 0})
+    return
+
+  payload = json.loads(event.object['message'].get('payload', '{}'))
+  if payload.get('id') == '1':
+    kick_user(vk_session, msg, kick_user_id)
--- main.py
+++ main.py
@@ -14,56 +14,50 @@ class MyLongPoll(VkBotLongPoll):
       except Exception as err:
         print(err)
 
+def handle_action_event(event, vk_session, msg, members):
+  if event.object['message']['action']['type'] == 'chat_kick_user':
+    selfkick_answer(vk_session, msg)
+  elif event.object['message']['action']['type'] == 'chat_invite_user':
+    if int(event.object['message']['action']['member_id']) > 0:
+      for profile in members['profiles']:
+        utils.get_user_by_profile(profile)
+    else:
+      for group in members['groups']:
+        utils.get_user_by_profile(group)
+
+def handle_text_event(event, vk_session, msg, user_id, admin_keys, members):
+  text_words = msg['text'].lower().split()
+  if 'кик' in text_words:
+    handle_kick(vk_session, msg, admin_keys, members)
+  elif 'снять' in text_words:
+    if 'преды' in text_words:
+      handle_remove_warns(vk_session, msg, members)
+    elif 'пред' in text_words:
+      handle_remove_warn(vk_session, msg, members)
+  elif 'пред' in text_words:
+    handle_warn(vk_session, msg, admin_keys, members)
+  else:
+    vk_session.method('messages.send', {
+      'chat_id': msg['peer_id'] - 2000000000,
+      'message': 'У вас нет уровня доступа для этих команд!',
+      'random_id': 0
+    })
+
 def runVkBot():
   vk_session = vk_api.VkApi(token=token)
-  longpoll = VkBotLongPoll(vk_session, group_id=group_id)
-
+  longpoll = MyLongPoll(vk_session, group_id=group_id)
+  
   for event in longpoll.listen():
     if event.type == VkBotEventType.MESSAGE_NEW:
       msg = event.obj.message
       user_id = msg['from_id']
-      text_words = msg['text'].lower().split()
-      # print(msg)
-      print(event)
-      
-      members = vk_session.method('messages.getConversationMembers', {
-        'peer_id': msg['peer_id']
-      })
-      
-      # САМОКИК
+      members = vk_session.method('messages.getConversationMembers', {'peer_id': msg['peer_id']})
+
       if 'action' in event.object['message']:
-        if event.object['message']['action']['type'] == 'chat_kick_user':
-          kick_user = event.object['message']['from_id']
-          selfkick_answer(vk_session, msg)
-        if event.object['message']['action']['type'] == 'chat_invite_user':
-          if int(event.object['message']['action']['member_id']) > 0:
-            for profile in members['profiles']:
-              utils.get_user_by_profile(profile)
-          else:
-            for group in members['groups']:
-              utils.get_user_by_profile(group)
-      elif 'Исключить' in event.object['message']['text']:
-        selfkick(vk_session, msg, event, user_id, kick_user)
-      else:
-        if user_id in map(int, admin_keys):
-          if 'кик' in text_words:
-            handle_kick(vk_session, msg, admin_keys, members)
-          elif 'снять' in text_words:
-            if 'преды' in text_words:
-              handle_remove_warns(vk_session, msg, members)
-            elif 'пред' in text_words:
-              handle_remove_warn(vk_session, msg, members)
-          elif 'пред' in text_words:
-            handle_warn(vk_session, msg, admin_keys, members)
-        else:
-        # if user_id not in map(int, admin_keys):
-          vk_session.method('messages.send', {
-            'chat_id': msg['peer_id'] - 2000000000,
-            'message': 'У вас нет уровня доступа для этих команд!',
-            'random_id': 0
-          })
-          continue
-      
+        handle_action_event(event, vk_session, msg, members)
+      elif user_id in map(int, admin_keys):
+        handle_text_event(event, vk_session, msg, user_id, admin_keys, members)
+        
       for profile in members['profiles']:
         if profile['id'] == user_id:
           utils.update_user_name(user_id, profile)
